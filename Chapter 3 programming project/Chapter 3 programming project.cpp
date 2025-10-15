df57d1ab91cc9ec1c2ddc9dfed84de9ad35faf24
longtphung-code <lphung15433@email.vccs.edu>
2025-10-15 13:15:30 -0400

    123

--- Chapter 3 programming project/Chapter 3 programming project.cpp
+++ Chapter 3 programming project/Chapter 3 programming project.cpp
@@ -8,11 +8,11 @@ int main()
     float s = 1.5; // s represents the cups of sugar per cookie
     float b = 1; // this represents the cups of butter per cookie
 	float f = 2.75; // this represents the cups of flour per cookie
-    int c; // this represents the number of cookies
-    std::cout << "Enter the number of cookies you want to make: ";
-    std::cin >> c;
-    std::cout << "You will need: \n";
-    std::cout << (s * c) / 48 << " cups of sugar\n";
+    int c; // this represents the number of cookies you want to make
+    std::cout << "Enter the number of cookies you want to make: "; // this line prints the message to the user
+    std::cin >> c; // this is used to take the user input and store it to the cariable c which represents the number of cookies you want to make
+    std::cout << "You will need: \n"; // prints the message to the user
+    std::cout << (s * c) / 48 << " cups of sugar\n"; // the next three lines calculate the amount of sugar, butter, and flour needed to make the number of cookies the user wants to make
     std::cout << (b * c) / 48 << " cups of butter\n";
 	std::cout << (f * c) / 48 << " cups of flour\n";
 
