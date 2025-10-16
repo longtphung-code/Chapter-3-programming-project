da3cb201779e4f6b18a01fd1eb7390042a3d84cc
longtphung-code <lphung15433@email.vccs.edu>
2025-10-15 21:31:35 -0400

    Update

--- Chapter 3 programming project/Chapter 3 programming project.cpp
+++ Chapter 3 programming project/Chapter 3 programming project.cpp
@@ -2,11 +2,24 @@
 //
 
 #include <iostream>
+#include <random>
 
-int main()
-{
+int main(){	
+	// random number generater
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distrib_int(1, 1000);
+
+	//generate two random numers
+	int num1 = distrib_int(gen);
+	int num2 = distrib_int(gen);
+	int correcAnswer = num1 + num2;
 
 
-
+	//take user input
+	std::cout << "what is " << num1 << " + " << num2 <<"?";
+	int userAnswer;
+	std::cin >> userAnswer;
    return 0;
    }
