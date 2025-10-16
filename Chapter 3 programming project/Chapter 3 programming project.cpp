3a3a9f95c6d834b700858405187daeb7c38270d1
longtphung-code <lphung15433@email.vccs.edu>
2025-10-15 23:33:10 -0400

    Final

--- Chapter 3 programming project/Chapter 3 programming project.cpp
+++ Chapter 3 programming project/Chapter 3 programming project.cpp
@@ -1,26 +1,28 @@
 
 #include <iostream>
-#include <random>
+#include <cmath>
 
 int main() {
 	// take user input for principal
 	std::cout << "What is your principal amount? ";
-	int principal;
+	double principal;
 	std::cin >> principal;
 
 	// take user input for rate
-	std::cout << "what is your interest rate?";
-	int rate;
-	std::cin >> rate; 
+	std::cout << "what is your interest rate in decimal form";
+	double rate;
+	std::cin >> rate;
 
+	// convert to decimal form
+	rate = rate / 10.0;
 
 	// take user input for times the interest is compounded during a year
 	std::cout << "How many times is the interest compounded during a year? ";
-	int timesCompounded;
+	double timesCompounded;
 	std::cin >> timesCompounded;
 
-	
-
-
+	double amount = principal * pow((1 + rate / timesCompounded), timesCompounded);
+	std::cout << "Your amount in savings is: " << amount << std::endl;
+	std::cout << "Your interest earned is: " << amount - principal << std::endl;
 	return 0;
 }
